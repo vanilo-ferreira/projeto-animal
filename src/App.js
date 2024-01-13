9bcd325a93ca98919217ee426404f0a149f1d262
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-01-12 23:12:06 -0300

    feat: Adicionar primeira versão do projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import imagemCachorro from './assets/cachorro.jpg';
+import imagemGato from './assets/gato.jpg';
+
 
 function App() {
+
+  let imagemAnimal = "";
+  let informacaoAnimal = "";
+  let tipoAnimal = "cachorro";
+
+  const [tipoDoComponenteCard, setTipoDoComponenteCard] = useState("cachorro");
+
+  const alterarState = () => {
+    if (tipoDoComponenteCard === "cachorro"){
+      setTipoDoComponenteCard("gato");
+    } else {
+      setTipoDoComponenteCard("cachorro");
+    }
+  }
+
+  if(tipoDoComponenteCard === "cachorro") {
+    imagemAnimal = imagemCachorro;
+    informacaoAnimal = "É um mamífero carnívoro da família dos canídeos.";
+    tipoAnimal = "cachorro";
+  } else {
+    imagemAnimal = imagemGato;
+    informacaoAnimal = "É um mamífero carnívoro da família dos felídeos.";
+    tipoAnimal = "Gato";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Projeto Props + useState</h1>
+        <p>Clique no botão para mudar os componentes abaixo</p>
+        <button onClick={alterarState}>Mudar Animal</button>
       </header>
+
+      <div id="cardAnimal">
+        <img src={imagemAnimal} alt="animal" />
+      </div>
+
+      <div className="CardInformacao">
+        <h3>Informação sobre o {tipoAnimal}</h3>
+        <p>{informacaoAnimal}</p>
+      </div>
+
     </div>
   );
 }
