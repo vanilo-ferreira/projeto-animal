fe8341c132a46f2d56ac8cb2a1acc0230fcf0c22
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-01-13 22:07:53 -0300

    fix: importar o componente Topo

--- src/App.js
+++ src/App.js
@@ -2,33 +2,21 @@ import './App.css';
 import React, { useState } from "react";
 import CardAnimal from './components/CardAnimal';
 import CardInformacoes from './components/CardInformacoes';
+import Topo from './components/Topo';
 
 
 function App() {
 
   const [tipoDoComponenteCard, setTipoDoComponenteCard] = useState("cachorro");
 
-  const alterarState = () => {
-    if (tipoDoComponenteCard === "cachorro") {
-      setTipoDoComponenteCard("gato");
-    } else {
-      setTipoDoComponenteCard("cachorro");
-    }
-  }
-
   return (
     <div className="App">
-      <header>
-        <h1>Projeto Props + useState</h1>
-        <p>Clique no botão para mudar os componentes abaixo</p>
-        <button onClick={alterarState}>Mudar Animal</button>
-      </header>
-
-      <CardAnimal tipoAnimal={tipoDoComponenteCard}/>
-      <CardInformacoes tipoAnimal={tipoDoComponenteCard}/>
-
+      <Topo tipoAnimal={tipoDoComponenteCard} setState={setTipoDoComponenteCard} />
+      <CardAnimal tipoAnimal={tipoDoComponenteCard} />
+      <CardInformacoes tipoAnimal={tipoDoComponenteCard} />
     </div>
   );
 }
+
 
 export default App;
